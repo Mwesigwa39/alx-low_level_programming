df86b32be633a0840f3a7e65fc6f80fe3244eebc
Mwesigwa Stephen <mwesigwa39@gmail.com>
2023-02-17 02:24:14 +0300

    UNIX corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
- * Program prints a string of characters
+ * Program prints a string of characters to the standard error.
  * Return: 1 (Success)
  */
+
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, 59); /*write to stderr*/
 	return (1);
 }
