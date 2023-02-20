e82eddddb312ad36a8593172399d364aeee9ca55
Mwesigwa Stephen <mwesigwa39@gmail.com>
2023-02-20 22:30:43 +0300

    Two digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: program prints two digit combinations
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+
+				if (a != '8' || (a == '8' && b != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
