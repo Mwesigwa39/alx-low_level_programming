e196f941b7afd349f53fb94ba8fd1d1066ead55b
Mwesigwa Stephen <mwesigwa39@gmail.com>
2023-02-27 22:56:48 +0300

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,9 @@
 
 /**
  * reset_to_98 - entry point
- * description - takes a pointer to an integer as parameter 
+ * description - takes a pointer to an integer as parameter
  * and updates the value it points to to 98.
+ * @n: integer input
  * Return: void
  */
 void reset_to_98(int *n)
